4467abbe5bf145b3604eefe8d96092f78766ad0b
vakharwalad23 <vakharwala.d@gmail.com>
2025-01-01 22:22:08 +0530

    feat(frontend): Integrate Tailwind CSS and PostCSS for styling

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
